9de5c4e7a72afb825611a3411ec185a020ae724a
Jerson Esteban <jerson014.jep@gmail.com>
2025-11-14 15:54:51 -0500

    feat(lesson-04): Create HousingLocation interface

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <img class="listing-photo" src="" alt="">
+      <h2 class="listing-heading"></h2>
+      <p class="listing-location"></p>
+    </section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
