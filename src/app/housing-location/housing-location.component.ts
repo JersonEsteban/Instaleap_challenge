ace1f625840fce37ef06a40e412e16690643d3fc
Jerson Esteban <jerson014.jep@gmail.com>
2025-11-14 23:03:08 -0500

    feat(lesson-10):Add routes to the application

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,16 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housingLocation';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}"> <!-- Interpolación lección 7 -->
       <h2 class="listing-heading">{{ housingLocation.name }}</h2> <!-- Interpolación  -->
       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>  <!-- Interpolación  -->
+      <a [routerLink]="['/details',housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrls: ['./housing-location.component.css']
